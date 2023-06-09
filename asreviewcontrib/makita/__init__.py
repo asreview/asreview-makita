7a97882c901ae67125409e83098ff6d5b2d84ce2
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-06-09 12:38:35 +0200

    Add __version__

---
+++ asreviewcontrib/makita/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from asreviewcontrib.makita._version import __version__
+    from asreviewcontrib.makita._version import __version_tuple__
+except ImportError:
+    __version__ = "0.0.0"
+    __version_tuple__ = (0, 0, 0)
