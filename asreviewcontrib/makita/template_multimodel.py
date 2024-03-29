04172cd3b136ad9db87354a6e0384c651e56580f
Jelle Teijema <j.j.teijema@gmail.com>
2024-03-29 15:48:48 +0100

    Start on multimodel class

--- asreviewcontrib/makita/template_multimodel.py
+++ asreviewcontrib/makita/template_multimodel.py
@@ -119,3 +119,42 @@ def render_jobs_multimodel(
             "impossible_models": [i.split(",") for i in impossible_models],
         }
     )
+
+"""Render multimodel template."""
+
+from asreviewcontrib.makita.template_base import RenderTemplateBase
+
+
+class RenderJobsMultiModel(RenderTemplateBase):
+    template_name = "multimodel"
+
+    def __init__(self, *args, **kwargs):
+        self.n_runs = kwargs.pop('n_runs', 1)
+        self.classifier = kwargs.pop('classifier', "nb")
+        self.feature_extractor = kwargs.pop('feature_extractor', "tfidf")
+        super().__init__(*args, **kwargs)
+
+    def prepare_dataset_params(self, index, fp_dataset):
+        return {
+            "input_file": fp_dataset.as_posix(),
+            "input_file_stem": fp_dataset.stem,
+            "model_seed": self.model_seed + index,
+            "init_seed": self.init_seed,
+            "n_runs": self.n_runs,
+        }
+
+    def prepare_template_params(self, params):
+        return {
+            "datasets": params,
+            "create_wordclouds": self.create_wordclouds,
+            "classifier": self.classifier,
+            "feature_extractor": self.feature_extractor,
+            "query_strategy": self.query_strategy,
+            "balance_strategy": self.balance_strategy,
+            "instances_per_query": self.instances_per_query,
+            "stop_if": self.stop_if,
+            "output_folder": self.output_folder,
+            "scripts_folder": self.scripts_folder,
+            "platform": self.platform_sys,
+            "version": self.__version__,
+        }
