0ead8413c51b7e54b3f7d842f0146bf7252be166
Jelle Teijema <j.j.teijema@gmail.com>
2024-04-03 17:23:08 +0200

    Update basic example

--- examples/basic_example/scripts/merge_tds.py
+++ examples/basic_example/scripts/merge_tds.py
@@ -24,6 +24,7 @@ Authors
 import argparse
 import glob
 import json
+from math import nan
 from pathlib import Path
 
 import pandas as pd
@@ -37,7 +38,7 @@ def create_table_state_tds(metrics):
         with open(metric) as f:
             i = next(filter(lambda x: x["id"] == "td", json.load(f)["data"]["items"]))[
                 "value"
-            ]  # noqa
+            ]
             values.extend((item[0], item[1], file_counter) for item in i)
             file_counter += 1
 
@@ -47,25 +48,26 @@ def create_table_state_tds(metrics):
         columns="metric_file",
         values="td",
         aggfunc="first",
-        fill_value=0,
+        fill_value=nan,
     )
     pivoted.columns = [f"td_sim_{col}" for col in pivoted.columns]
     return pivoted
+
+
+def get_atd_values(df):
+    df["record_atd"] = df.mean(axis=1)
+
+    df.loc["average_simulation_TD"] = df.iloc[:, :-1].mean(axis=0)
+
+    return df
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Merge tds of multiple metrics into single table."
     )
-    parser.add_argument(
-        "-s", type=str, default="output/simulation/*/metrics/", help="metrics location"
-    )
-    parser.add_argument(
-        "-o",
-        type=str,
-        default="output/tables/tds_sim_all.csv",
-        help="Output table location",
-    )
+    parser.add_argument("-s", type=str, required=True, help="metrics location")
+    parser.add_argument("-o", type=str, required=True, help="Output table location")
     args = parser.parse_args()
 
     # load metric files
@@ -75,9 +77,14 @@ if __name__ == "__main__":
     if len(metric_files) == 0:
         raise FileNotFoundError("No metrics found in " + args.s)
 
-    states_table = create_table_state_tds(metric_files)
+    # check if output file has .csv extension
+    if Path(args.o).suffix != ".csv":
+        raise ValueError("Output file should have .csv extension")
+
+    td_table = create_table_state_tds(metric_files)
+    atd_table = get_atd_values(td_table)
 
     # store table
     Path(args.o).parent.mkdir(parents=True, exist_ok=True)
-    states_table.to_csv(Path(args.o))
-    states_table.to_excel(Path(args.o).with_suffix(".xlsx"))
+    atd_table.to_csv(Path(args.o))
+    atd_table.to_excel(Path(args.o).with_suffix(".xlsx"))
