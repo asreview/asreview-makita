eb7b7e94617a50fd3ca77e988f26f4305d319302
jteijema <j.j.teijema@uu.nl>
2024-09-17 18:35:58 +0200

    ruff formatting

--- asreviewcontrib/makita/template_prior.py
+++ asreviewcontrib/makita/template_prior.py
@@ -8,7 +8,7 @@ from asreview.data import load_data
 from asreviewcontrib.makita.template_base import TemplateBase
 
 # Suppress FutureWarning messages
-warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
 class TemplatePrior(TemplateBase):
@@ -39,14 +39,18 @@ class TemplatePrior(TemplateBase):
         asreview_data = load_data(fp_dataset)
 
         # Create a DataFrame with the desired columns: label, abstract, and title
-        dataset = pd.DataFrame({
-            'title': asreview_data.title,
-            'abstract': asreview_data.abstract,
-            'label': asreview_data.labels,
-        })
+        dataset = pd.DataFrame(
+            {
+                "title": asreview_data.title,
+                "abstract": asreview_data.abstract,
+                "label": asreview_data.labels,
+            }
+        )
 
         # Add the 'makita_priors' column
-        if fp_dataset.name.startswith("prior_") or fp_dataset.name.startswith("priors_"):
+        if fp_dataset.name.startswith("prior_") or fp_dataset.name.startswith(
+            "priors_"
+        ):
             dataset["makita_priors"] = 1
             self._prior_dataset_count += 1
         else:
@@ -62,23 +66,46 @@ class TemplatePrior(TemplateBase):
         """Prepare template-specific parameters. These parameters are provided to the
         template only once."""
 
-        classifier = self.classifier if self.classifier is not None else ASREVIEW_CONFIG.DEFAULT_MODEL
-        feature_extractor = self.feature_extractor if self.feature_extractor is not None else ASREVIEW_CONFIG.DEFAULT_FEATURE_EXTRACTION
-        query_strategy = self.query_strategy if self.query_strategy is not None else ASREVIEW_CONFIG.DEFAULT_QUERY_STRATEGY
-        balance_strategy = self.balance_strategy if self.balance_strategy is not None else ASREVIEW_CONFIG.DEFAULT_BALANCE_STRATEGY
+        classifier = (
+            self.classifier
+            if self.classifier is not None
+            else ASREVIEW_CONFIG.DEFAULT_MODEL
+        )
+        feature_extractor = (
+            self.feature_extractor
+            if self.feature_extractor is not None
+            else ASREVIEW_CONFIG.DEFAULT_FEATURE_EXTRACTION
+        )
+        query_strategy = (
+            self.query_strategy
+            if self.query_strategy is not None
+            else ASREVIEW_CONFIG.DEFAULT_QUERY_STRATEGY
+        )
+        balance_strategy = (
+            self.balance_strategy
+            if self.balance_strategy is not None
+            else ASREVIEW_CONFIG.DEFAULT_BALANCE_STRATEGY
+        )
         n_runs = self.n_runs if self.n_runs is not None else 1
 
         # Check if at least one dataset with prior knowledge is present
         if self._prior_dataset_count == 0:
-            raise ValueError("At least one dataset with prior knowledge (prefix 'prior_' or 'priors_') is required.")
+            raise ValueError(
+                "At least one dataset with prior knowledge (prefix 'prior_' or \
+                    'priors_') is required."
+            )
 
         # Check if at least one dataset without prior knowledge is present
         if self._non_prior_dataset_count == 0:
-            raise ValueError("At least one dataset without prior knowledge is required.")
+            raise ValueError(
+                "At least one dataset without prior knowledge is required."
+            )
 
         # Print the number of datasets with and without prior knowledge
         print(f"\nTotal datasets with prior knowledge: {self._prior_dataset_count}")
-        print(f"Total datasets without prior knowledge: {self._non_prior_dataset_count}")
+        print(
+            f"Total datasets without prior knowledge: {self._non_prior_dataset_count}"
+        )
 
         # Create a directory for generated data if it doesn't already exist
         generated_folder = Path("generated_data")
@@ -90,11 +117,17 @@ class TemplatePrior(TemplateBase):
 
         # Combine all datasets into one DataFrame and remove rows where label is -1
         combined_dataset = pd.concat(self.prior_makita_datasets, ignore_index=True)
-        combined_dataset.drop(combined_dataset[combined_dataset.label == -1].index, inplace=True)
+        combined_dataset.drop(
+            combined_dataset[combined_dataset.label == -1].index, inplace=True
+        )
 
         # Calculate the total number of rows with and without prior knowledge
-        total_rows_with_priors = combined_dataset[combined_dataset["makita_priors"] == 1].shape[0]
-        total_rows_without_priors = combined_dataset[combined_dataset["makita_priors"] == 0].shape[0]
+        total_rows_with_priors = combined_dataset[
+            combined_dataset["makita_priors"] == 1
+        ].shape[0]
+        total_rows_without_priors = combined_dataset[
+            combined_dataset["makita_priors"] == 0
+        ].shape[0]
 
         # Print the number of rows with and without prior knowledge
         print(f"Total rows of prior knowledge: {total_rows_with_priors}")
@@ -102,12 +135,19 @@ class TemplatePrior(TemplateBase):
 
         # Save the combined dataset to the appropriate file paths
         combined_dataset.to_csv(filepath_with_priors, index=False)
-        combined_dataset[combined_dataset["makita_priors"] != 1].to_csv(filepath_without_priors, index=False)
+        combined_dataset[combined_dataset["makita_priors"] != 1].to_csv(
+            filepath_without_priors, index=False
+        )
 
         # Create a string of indices for rows with prior knowledge
-        prior_idx_list = combined_dataset[combined_dataset["makita_priors"] == 1].index.tolist()
+        prior_idx_list = combined_dataset[
+            combined_dataset["makita_priors"] == 1
+        ].index.tolist()
         if len(prior_idx_list) != total_rows_with_priors:
-            raise ValueError("prior_idx list is not equal in length to rows of prior knowledge")
+            raise ValueError(
+                "prior_idx list is not equal in length to rows of prior \
+                             knowledge"
+            )
         prior_idx = " ".join(map(str, prior_idx_list))
 
         return {
@@ -129,5 +169,5 @@ class TemplatePrior(TemplateBase):
             "filepath_with_priors_stem": filepath_with_priors.stem,
             "filepath_without_priors": filepath_without_priors,
             "filepath_without_priors_stem": filepath_without_priors.stem,
-            "prior_idx": prior_idx
+            "prior_idx": prior_idx,
         }
