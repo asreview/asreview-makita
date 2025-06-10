b4f239ed3d6460cabe2157153d437e996dcd13a8
Jonathan de Bruin <jonathandebruinos@gmail.com>
2025-06-10 17:19:30 +0200

    Use temp folder for downloading data

--- scripts/generate_examples.sh
+++ scripts/generate_examples.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
+# Use a temporary folder for examples/data
+DATA_DIR=$(mktemp -d)
+
 # Download datasets
-synergy_dataset get -d van_de_Schoot_2018 Smid_2020 -o examples/data -l
+synergy_dataset get -d van_de_Schoot_2018 Smid_2020 -o "$DATA_DIR" -l
 
 # Rename file for prior example
-mv ./examples/data/Smid_2020.csv ./examples/data/prior_Smid_2020.csv
+mv "$DATA_DIR/Smid_2020.csv" "$DATA_DIR/prior_Smid_2020.csv"
 
 # Generate example templates
-asreview makita template arfi -d ./examples/data -p ./examples/arfi_example --overwrite --platform linux
-asreview makita template basic -d ./examples/data -p ./examples/basic_example --overwrite --platform linux
-asreview makita template multimodel -d ./examples/data -p ./examples/multimodel_example --overwrite --platform linux
-asreview makita template prior -d ./examples/data -p ./examples/prior_example --overwrite --platform linux
+asreview makita template arfi -d "$DATA_DIR" -p ./examples/arfi_example --overwrite --platform linux
+asreview makita template basic -d "$DATA_DIR" -p ./examples/basic_example --overwrite --platform linux
+asreview makita template multimodel -d "$DATA_DIR" -p ./examples/multimodel_example --overwrite --platform linux
+asreview makita template prior -d "$DATA_DIR" -p ./examples/prior_example --overwrite --platform linux
