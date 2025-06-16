c3a5739ba2c217dcf09986475c85686ff1a05f71
Jelle Teijema <j.j.teijema@uu.nl>
2025-06-16 15:55:18 +0200

    Update generate_examples.sh with more explicit prior template folder

--- scripts/generate_examples.sh
+++ scripts/generate_examples.sh
@@ -10,11 +10,17 @@ DATA_DIR=$(mktemp -d)
 # Download datasets
 synergy_dataset get -d van_de_Schoot_2018 Smid_2020 -o "$DATA_DIR" -l
 
-# Rename file for prior example
-mv "$DATA_DIR/Smid_2020.csv" "$DATA_DIR/prior_Smid_2020.csv"
-
-# Generate example templates
+# Generate arfi, basic, and multimodel examples
 asreview makita template arfi -d "$DATA_DIR" -p ./examples/arfi_example --overwrite --platform linux
 asreview makita template basic -d "$DATA_DIR" -p ./examples/basic_example --overwrite --platform linux
 asreview makita template multimodel -d "$DATA_DIR" -p ./examples/multimodel_example --overwrite --platform linux
-asreview makita template prior -d "$DATA_DIR" -p ./examples/prior_example --overwrite --platform linux
+
+# Copy data for prior template example
+PRIOR_DATA_DIR=$(mktemp -d)
+cp "$DATA_DIR/van_de_Schoot_2018.csv" "$PRIOR_DATA_DIR/"
+cp "$DATA_DIR/Smid_2020.csv" "$PRIOR_DATA_DIR/prior_Smid_2020.csv"
+
+# Generate prior template example
+asreview makita template prior -d "$PRIOR_DATA_DIR" -p ./examples/prior_example --overwrite --platform linux
+
+rm -rf "$DATA_DIR" "$PRIOR_DATA_DIR"
