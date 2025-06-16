5d83519da3b2e4773d934efb3c51192b8ad6ccb4
Jonathan de Bruin <jonathandebruinos@gmail.com>
2025-06-16 16:22:38 +0200

    Add option to override version for examples

--- asreviewcontrib/makita/__init__.py
+++ asreviewcontrib/makita/__init__.py
@@ -1,6 +1,16 @@
-try:
-    from asreviewcontrib.makita._version import __version__
-    from asreviewcontrib.makita._version import __version_tuple__
-except ImportError:
-    __version__ = "0.0.0"
-    __version_tuple__ = (0, 0, 0)
+import os
+
+env_version = os.environ.get("ASREVIEW_MAKITA_VERSION_OVERRIDE")
+if env_version is not None:
+    __version__ = env_version
+    try:
+        __version_tuple__ = tuple(int(x) for x in env_version.split("."))
+    except Exception:
+        __version_tuple__ = (0, 0, 0)
+else:
+    try:
+        from asreviewcontrib.makita._version import __version__
+        from asreviewcontrib.makita._version import __version_tuple__
+    except ImportError:
+        __version__ = "0.0.0"
+        __version_tuple__ = (0, 0, 0)
