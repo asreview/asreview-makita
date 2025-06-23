4b8b64bc8619d32d5f003d9633ae988b55ece395
Jonathan de Bruin <jonathandebruinos@gmail.com>
2025-06-23 10:45:28 +0200

    Rename multimodel template into modelmatrix (#101)

--- scripts/generate_examples.sh
+++ scripts/generate_examples.sh
@@ -12,12 +12,12 @@ DATA_DIR=$(mktemp -d)
 # Download datasets
 synergy_dataset get -d van_de_Schoot_2018 Smid_2020 -o "$DATA_DIR" -l
 
-# Generate basic, arfi, and multimodel examples
+# Generate basic, arfi, and modelmatrix examples
 asreview makita template basic -d "$DATA_DIR" -p ./examples/basic_example --overwrite --platform linux
 asreview makita template basic -d "$DATA_DIR" -p ./examples/basic_multi_ai_example --overwrite --platform linux --ai elas_u3 elas_u4
 asreview makita template arfi -d "$DATA_DIR" -p ./examples/arfi_example --overwrite --platform linux
 asreview makita template arfi -d "$DATA_DIR" -p ./examples/arfi_multi_ai_example --overwrite --platform linux --ai elas_u3 elas_u4
-asreview makita template multimodel -d "$DATA_DIR" -p ./examples/multimodel_example --overwrite --platform linux
+asreview makita template modelmatrix -d "$DATA_DIR" -p ./examples/modelmatrix_example --overwrite --platform linux
 
 # Copy data for prior template example
 PRIOR_DATA_DIR=$(mktemp -d)
